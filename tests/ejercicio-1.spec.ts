87cdb78c4d8336253f0805bd943940fba6d083d1
Nitro1000 <alu0101160276@ull.edu.es>
2021-03-31 02:59:16 +0100

    Prueba ejercicio 1 falla: no se han creado las clases

--- tests/ejercicio-1.spec.ts
+++ tests/ejercicio-1.spec.ts
@@ -1,13 +1,19 @@
 import 'mocha';
 import { expect } from 'chai';
-import { } from '../src/ejercicio-1/';
-import { } from '../src/ejercicio-1/';
+import { Combat } from '../src/ejercicio-1/combat';
+import { Pokemon } from '../src/ejercicio-1/pokemon';
+import { DragonBall } from '../src/ejercicio-1/dragonball';
+import { Digimon } from '../src/ejercicio-1/digimon';
+
+
 
 describe('El combate definitivo', () => {
-  describe('El combate definitivo', () => {
-    it('', () => {
-      const a;
-      expect().to.eql('');
+  describe('Prueba de las clases', () => {
+    it('Combate entre Goku y Pikachu', () => {
+      const gerrero = new DragonBall('Goku', 'kamehameha', ['Pokemon', 'Digimon'], 1000, 500, 200, 400);
+      const pokemon = new Pokemon('Pikachu', 'electrico', 'pika pika chuuuu', ['Dragon', 'Digimon'], 2000, 100, 50, 100);
+      const fight = new Combat(gerrero, pokemon);
+      expect(fight.fight()).to.eql('Ganó: Goku');
     });
   });
 });
